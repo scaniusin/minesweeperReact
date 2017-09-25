352745339867ad1154634a502f21f8115e2fc855
scaniusin <scaniusin@gmail.com>
2017-09-25 14:18:13 +0300

    fix problems

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './utils';
-import Table from "./Comonents/Table";
+import Table from "./Components/Table";
 
 class App extends Component {
   constructor(props){
